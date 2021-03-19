06477b775c08cdcc37f6fab0fde91aeeab33880b
jacekv <jaca347@gmail.com>
2021-03-19 11:50:50 +0100

    Fixing solidity coverage error

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,7 @@
 require("@nomiclabs/hardhat-waffle");
 require("solidity-coverage");
-const fs = require('fs')
+const fs = require('fs');
+const { exit } = require("process");
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -14,16 +15,16 @@ task("accounts", "Prints the list of accounts", async () => {
 
 function readPrivateKey(name) {
   try {
-    const data = fs.readFileSync(`privkeys/${name}.key`, 'utf8')
+    const data = fs.readFileSync(`privkeys/${name}.key`, 'utf8');
     return data;
   } catch (err) {
-    console.error(err)
+    console.error(err.message);
+    exit(1);
   }
 }
 
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
-
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
